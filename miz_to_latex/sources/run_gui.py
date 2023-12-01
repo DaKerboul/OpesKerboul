0017631ad0fc31dbc9a582ff5de44c0c46634021
Kerboul <31707223+DaKerboul@users.noreply.github.com>
2023-12-01 12:02:30 +0100

    Mise à jour du script run_gui.py
    Cette mise à jour apporte plusieurs améliorations et corrections au script run_gui.py pour la génération de documents LaTeX à partir des briefings de missions DCS World. Les changements incluent :
    
    1. Correction de la gestion des caractères spéciaux pour éviter les erreurs LaTeX.
    2. Utilisation de l'environnement verbatim pour conserver les crochets tels quels dans le document LaTeX.
    3. Ajout de la recherche et de l'intégration des informations provenant de DictKey_descriptionText_1.
    4. Vérification de l'égalité entre match_mission_name et match_mission_name2 pour title_check.
    5. Ajustements pour rendre le code plus robuste et éviter les erreurs de compilation LaTeX.
    
    Ces modifications améliorent la stabilité et la convivialité du script lors de la génération de documents LaTeX à partir des briefings DCS World.

--- miz_to_latex/sources/run_gui.py
+++ miz_to_latex/sources/run_gui.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
@@ -25,13 +27,21 @@ def extraire_et_creer_latex():
         # Utilisation de regex pour extraire les informations spécifiques
         pattern_mission_name = r'\["DictKey_missionName"\]\s*=\s*"([^"]*)"'
         pattern_editor_notes = r'\["DictKey_briefing"\]\s*=\s*"([^"]*)"'
+        pattern_description_text_1 = r'\["DictKey_descriptionText_1"\]\s*=\s*"([^"]*)"'
 
         match_mission_name = re.search(pattern_mission_name, contenu)
         match_editor_notes = re.search(pattern_editor_notes, contenu)
+        match_description_text_1 = re.search(pattern_description_text_1, contenu)
 
-        if match_editor_notes and match_mission_name:
+        if match_mission_name and (match_editor_notes or match_description_text_1):
             mission_name = match_mission_name.group(1).replace('\\', '\\\\')
-            editor_notes = match_editor_notes.group(1).replace('\\', '\\\\')
+            editor_notes = match_editor_notes.group(1).replace('\\', '\\\\') if match_editor_notes else ""
+            description_text_1 = match_description_text_1.group(1).replace('\\',
+                                                                           '\\\\') if match_description_text_1 else ""
+            editor_notes = editor_notes + description_text_1
+
+            mission_name = mission_name.replace('_', r'\_')
+            editor_notes = editor_notes.replace('_', r'\_')
 
             # Demander à l'utilisateur le nom du fichier LaTeX de sortie
             nom_fichier_latex = filedialog.asksaveasfilename(defaultextension=".tex",
